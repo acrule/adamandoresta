457076aa0435c365af3f49337108e93dbfc9c863
acrule <acrule@gmail.com>
2017-11-26 08:55:36 -0800

    wedding photos

--- js/img.js
+++ js/img.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $("#hero_img").fadeOut(100, function() {
-      var num = Math.round(Math.random()*3)+1;
+      var num = Math.round(Math.random()*3)+5;
       var imgName = "img/hero_" + num.toString() + ".jpg";
       $(this).attr("src", imgName);
   }).fadeIn(500);
