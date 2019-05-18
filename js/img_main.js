846f9a93b1c00e72811ff857aa9628e8c7f50385
Adam Rule <adam@Adams-MacBook-Air-2.local>
2019-05-18 15:10:57 -0700

    update personal site to be more generic

---
+++ js/img_main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $("#hero_img").fadeOut(100, function() {
+      var imgsNums = [1, 3, 9];
+      var imgNum = imgsNums[Math.floor(Math.random()*imgsNums.length)];
+      var imgName = "img/hero_" + imgNum.toString() + ".jpg";
+      console.log(imgName);
+      $(this).attr("src", imgName);
+  }).fadeIn(500);
+    return false;
+});
