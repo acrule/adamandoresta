ecb2ab03e26f73c6f3f7ed607c15c5626ffc59c9
Adam Rule <adam@Adams-MacBook-Air-2.local>
2019-05-21 16:12:46 -0700

    add hero image

--- js/img_main.js
+++ js/img_main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $("#hero_img").fadeOut(100, function() {
-      var imgsNums = [1, 3, 9];
+      var imgsNums = [1, 3, 9, 10];
       var imgNum = imgsNums[Math.floor(Math.random()*imgsNums.length)];
       var imgName = "img/hero_" + imgNum.toString() + ".jpg";
       console.log(imgName);
