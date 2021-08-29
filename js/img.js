53643074f0083a0cc5d9b066ff56fbbd95e02ad5
Adam Rule <acrule@gmail.com>
2021-08-29 16:51:38 -0500

    Delete img.js

--- js/img.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $("#hero_img").fadeOut(100, function() {
-      var num = Math.round(Math.random()*3)+5;
-      var imgName = "img/hero_" + num.toString() + ".jpg";
-      $(this).attr("src", imgName);
-  }).fadeIn(500);
-    return false;
-});
